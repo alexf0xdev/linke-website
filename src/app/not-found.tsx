c06c0f7605d5bacd1631a8c4bb40aecd4e899f7d
alexf0xdev <glebf78@gmail.com>
2024-10-05 02:47:05 +0500

    fix bugs

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -11,7 +11,7 @@ const NotFound = () => {
     <div className={cn('flex flex-col items-center justify-center h-screen')}>
       <h1 className={cn('text-5xl sm:text-6xl text-center')}>Page not found</h1>
       <p className={cn('text-xl sm:text-2xl text-center mt-5')}>
-        We didn't find any such page, but there are many others out there
+        We didn&apos;t find any such page, but there are many others out there
       </p>
       <div className={cn('flex justify-center mt-10')}>
         <Button href='/'>Go to Home</Button>
